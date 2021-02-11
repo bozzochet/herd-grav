f87fbbb22cb7cd0dcb6bc49ee0290b3659674e7b
Matteo Duranti <bozzo1983@gmail.com>
2021-02-11 13:06:17 +0100

    up

---
+++ install.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+#taken from: https://learn.getgrav.org/16/basics/installation
+composer install --no-dev -o
+bin/grav install
+
+#to install used plugins:
+
+#this on the final server should be not needed
+bin/gpm install admin
+
+bin/gpm install bibtexify
+
+bin/gpm install consistent-backup-name
+
+bin/gpm install facebook-feed
+
+bin/gpm install pdf-js
