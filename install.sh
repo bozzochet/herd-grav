eabf32ad9d27dd0d564c133f1d588fba8b2016ce
Matteo Duranti <bozzo1983@gmail.com>
2021-02-12 16:59:58 +0100

    up

--- install.sh
+++ install.sh
@@ -5,18 +5,4 @@ composer install --no-dev -o
 bin/grav install
 
 #to install used plugins:
-
-#this on the final server should be not needed
-bin/gpm install admin
-
-bin/gpm install bibtexify
-
-bin/gpm install consistent-backup-name
-
-bin/gpm install facebook-feed
-
-bin/gpm install pdf-js
-
-bin/gpm install devtools
-
-bin/gpm install page-inject
+source ./install_plugins.sh
---
+++ install_plugins.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+#this on the final server should be not needed
+bin/gpm install admin
+
+bin/gpm install bibtexify
+
+bin/gpm install consistent-backup-name
+
+bin/gpm install facebook-feed
+
+bin/gpm install pdf-js
+
+bin/gpm install devtools
+
+bin/gpm install page-inject
