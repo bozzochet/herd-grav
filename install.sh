b3f767a247e63182ee824f795c0d300054361a74
Matteo Duranti <bozzo1983@gmail.com>
2021-02-13 14:02:24 +0100

    up

--- install.sh
+++ install.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
 #taken from: https://learn.getgrav.org/16/basics/installation
+
 #composer install --no-dev -o
+./install_composer.sh
 ./composer.phar install --no-dev -o
-bin/grav install -y
+
+bin/grav install
 
 #to install used plugins:
 ./install_plugins.sh
