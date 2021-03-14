063220d2f08b551fcd7a3b21ac59dc02f4619914
Matteo Duranti <bozzo1983@gmail.com>
2021-03-14 12:52:05 +0100

    up

--- remove_plugins.sh
+++ remove_plugins.sh
@@ -1,6 +1,15 @@
 #!/bin/bash -l
 
+bin/gpm uninstall -y facebook-feed
+
+bin/gpm uninstall -y form
+
+bin/gpm uninstall -y login
+
+bin/gpm uninstall -y email
+
+bin/gpm uninstall -y flex-objects
+
 #this on the final server should be removed
 bin/gpm uninstall -y admin
 
-bin/gpm uninstall -y facebook-feed
